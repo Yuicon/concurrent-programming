5ed1255cd6893447d7d8485ddf8bf30258bc0839
Yuicon <910722178@qq.com>
2018-02-08 22:17:09 +0800

    3-3

---
+++ src/chapterthree/SynchronizedInteger.java
@@ -0,0 +1,20 @@
+package chapterthree;
+
+import annotation.GuardedBy;
+import annotation.ThreadSafe;
+
+/**
+ * @author Yuicon
+ */
+@ThreadSafe
+public class SynchronizedInteger {
+    @GuardedBy("this") private int value;
+
+    public synchronized int get() {
+        return value;
+    }
+
+    public synchronized void set(int value) {
+        this.value = value;
+    }
+}
