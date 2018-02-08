bb1797cecf5336535052e96d6921cf47bf863597
Yuicon <910722178@qq.com>
2018-02-08 22:15:13 +0800

    3-2

---
+++ src/chapterthree/MutableInteger.java
@@ -0,0 +1,19 @@
+package chapterthree;
+
+import annotation.NotThreadSafe;
+
+/**
+ * @author Yuicon
+ */
+@NotThreadSafe
+public class MutableInteger {
+    private int value;
+
+    public int get() {
+        return value;
+    }
+
+    public void set(int value) {
+        this.value = value;
+    }
+}
